c1e506ab9cf0290243cf9e2b06bb5d43f4426de3
Maxim Salnikov <salnikov@gmail.com>
2022-10-05 09:54:36 +0200

    sw-cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,28 @@
+const VERSION = 'v1';
 self.addEventListener('install', function (event) {
-  console.log('installing');
+  event.waitUntil(
+    caches.open(VERSION).then(function (cache) {
+      return cache.addAll(['/css/main.css', '/js/main.js']);
+    })
+  );
+  self.skipWaiting();
 });
-self.addEventListener('activate', function (event) {
-  console.log('activating');
+
+self.addEventListener('activate', (event) => {
+  // clean up stale caches
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => {
+            return !key.startsWith(VERSION);
+          })
+          .map((key) => {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+
+  clients.claim();
 });
